4bc75baf1cf56e51a61c42dc02e1b2d670db6fa5
oadam <olivier.adam@m4x.org>
2012-12-15 10:35:25 +0100

    find return a slice

--- dico.go
+++ dico.go
@@ -34,34 +34,43 @@ func (runes sortRunes) Swap(i, j int) {
 	runes[i], runes[j] = runes[j], runes[i]
 }
 
+type sortResults []string
+
+func (result sortResults) Len() int {
+	return len(result)
+}
+func (result sortResults) Less(i, j int) bool {
+	if len(result[i]) > len(result[j]) {return true}
+	return result[i] < result[j]
+}
+func (result sortResults) Swap(i, j int) {
+	result[i], result[j] = result[j], result[i]
+}
+
 type Dico node
 
-func (d *Dico) Find(letters string) chan string {
-	var resultChan = make(chan string)
-	go func() {
-		var sorted = wordToSortedRunes(letters)
-		var root = node(*d)
-		var currents = []*node{&root}
-		var sent = map[*node]bool{}
-		for _, r := range sorted {
-			var nexts = []*node{}
-			for _, current := range currents {
-				var next = current.children[r]
-				if next != nil {
-					nexts = append(nexts, next)
-					if !sent[next] {
-						sent[next] = true
-						for _, word := range next.words {
-							resultChan <- word
-						}
-					}
+func (d *Dico) Find(letters string) []string {
+	var result []string
+	var sorted = wordToSortedRunes(letters)
+	var root = node(*d)
+	var currents = []*node{&root}
+	var seen = map[*node]bool{}
+	for _, r := range sorted {
+		var nexts = []*node{}
+		for _, current := range currents {
+			var next = current.children[r]
+			if next != nil && !seen[next] {
+				nexts = append(nexts, next)
+				seen[next] = true
+				for _, word := range next.words {
+					result = append(result, word)
 				}
 			}
-			currents = append(currents, nexts...)
 		}
-		close(resultChan)
-	}()
-	return resultChan
+		currents = append(currents, nexts...)
+	}
+	sort.Sort(sortResults(result))
+	return result
 }
 
 func wordToSortedRunes(word string) []rune {
--- dico_test.go
+++ dico_test.go
@@ -1,29 +1,37 @@
 package dico
 
 import (
-	"fmt"
 	"os"
 	"testing"
+	"strings"
 )
 
 func TestRune(t *testing.T) {
-	var truc = wordToSortedRunes("azertyuiopqsdfghjklmwxcvbn")
-	fmt.Println(string(truc))
+	var truc = string(wordToSortedRunes("azertyuiopqsdfghjklmwxcvbn"))
+	const expected = "abcdefghijklmnopqrstuvwxyz"
+	if truc != expected {
+		t.Errorf("expected %s but received %s", expected, truc)
+	}
 }
+const exampleWords = "a\nab\naba\ntoto\n"
 
 func TestNew(t *testing.T) {
-	file, err := os.Open("test_mots.txt")
-	defer file.Close()
-	if err != nil {
-		panic(err)
-	}
+	file := strings.NewReader(exampleWords)
 	var d = New(file)
 	var n = node(d)
 	if _, ok := n.children['a']; !ok {
-		fmt.Printf("no node for a, map contains %v", len(n.children))
+		t.Errorf("no node for a, map contains %v", len(n.children))
 	}
 	if words := n.children['a'].words; len(words) != 1 {
-		fmt.Printf("expected 1 word but found %v", words)
+		t.Errorf("expected 1 word but found %v", words)
+	}
+}
+func TestSimpleFind(t *testing.T) {
+	file := strings.NewReader(exampleWords)
+	var d = New(file)
+	var results = d.Find("baa")
+	if len(results) != 3 {
+		t.Errorf("expected 3 results but received %v", results)
 	}
 }
 func TestFind(t *testing.T) {
