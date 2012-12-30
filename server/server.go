b55d3d239477fe7867b4d05617cffe19b721ac6b
oadam <olivier.adam@m4x.org>
2012-12-30 05:00:00 +0100

    log processing times

--- server/server.go
+++ server/server.go
@@ -4,6 +4,7 @@ import (
 	"dico/dico"
 	"encoding/json"
 	"log"
+	"time"
 	"net/http"
 	"fmt"
 	"os"
@@ -19,14 +20,17 @@ func main() {
 	var d = dico.New(file)
 
 	http.Handle("/query", http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		var receivedAt = time.Now()
 		req.ParseForm()
 		var query = req.Form.Get("q")
 		if len(query) > maxLetters {
 			w.WriteHeader(http.StatusBadRequest)
 			fmt.Fprintln(w, "Too much (more than", maxLetters,") letters")
+			query = query[0:maxLetters] + "..."
 		} else {
 			json.NewEncoder(w).Encode(d.Find(query))
 		}
+		log.Println("handled request \"",query,"\" in", time.Since(receivedAt))
 	}))
 	http.Handle("/", http.FileServer(http.Dir("resources")))
 
