18135754d3c7135e4fbb37defcb1ecc845671de4
oadam <olivier.adam@m4x.org>
2012-12-30 11:46:46 +0100

    extracted handler func

--- server/server.go
+++ server/server.go
@@ -3,12 +3,13 @@ package main
 import (
 	"dico/dico"
 	"encoding/json"
-	"log"
-	"time"
-	"net/http"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
+	"time"
 )
+
 const maxLetters = 12
 
 func main() {
@@ -19,19 +20,7 @@ func main() {
 	defer file.Close()
 	var d = dico.New(file)
 
-	http.Handle("/query", http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		var receivedAt = time.Now()
-		req.ParseForm()
-		var query = req.Form.Get("q")
-		if len(query) > maxLetters {
-			w.WriteHeader(http.StatusBadRequest)
-			fmt.Fprintln(w, "Too much (more than", maxLetters,") letters")
-			query = query[0:maxLetters] + "..."
-		} else {
-			json.NewEncoder(w).Encode(d.Find(query))
-		}
-		log.Println("handled request \"",query,"\" in", time.Since(receivedAt))
-	}))
+	http.Handle("/query", handler(d))
 	http.Handle("/", http.FileServer(http.Dir("resources")))
 
 	var port = ":" + os.Getenv("PORT")
@@ -40,4 +29,22 @@ func main() {
 	if err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
+}
+
+type handler dico.Dico
+
+func (h handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	var d dico.Dico = dico.Dico(h);
+	var receivedAt = time.Now()
+	req.ParseForm()
+	var query = req.Form.Get("q")
+	if len(query) > maxLetters {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(w, "Too much (more than", maxLetters, ") letters")
+		query = query[0:maxLetters] + "..."
+	} else {
+		var result = (&d).Find(query)
+		json.NewEncoder(w).Encode(result)
+	}
+	log.Println("handled request \"", query, "\" in", time.Since(receivedAt))
 }
